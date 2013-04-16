41db87dd2963b8e94f5c6a2d75d5611a118aac34
Aaron Quint <aaron@quirkey.com>
2013-04-16 17:42:05 -0400

    Fix size of image

--- magick_test.go
+++ magick_test.go
@@ -91,7 +91,7 @@ func TestToBlob(t *testing.T) {
 	assert.T(t, err == nil)
 	assert.T(t, bytes != nil)
 	assert.T(t, len(bytes) > 0)
-	assert.Equal(t, 477575, len(bytes))
+	assert.Equal(t, 437198, len(bytes))
 }
 
 func TestToFile(t *testing.T) {
@@ -106,7 +106,7 @@ func TestToFile(t *testing.T) {
 	defer file.Close()
 	stat, err := file.Stat()
 	assert.T(t, stat != nil)
-	assert.Equal(t, (int64)(477575), stat.Size())
+	assert.Equal(t, (int64)(437198), stat.Size())
 }
 
 func TestFullStack(t *testing.T) {
